29ea800d884d824d783377b36c01f1184d239a2b
Sulstice <sharifsuliman1@gmail.com>
2023-05-11 15:31:04 -0400

    ss-main: universes

---
+++ machine_learning_operations/food/smiles_generator.py
@@ -0,0 +1,37 @@
+# Imports
+# -------
+
+from rdkit import Chem
+from global_chem import GlobalChem
+
+
+if __name__ == '__main__':
+
+    output_file = open('mol.smi', 'w')
+    gc = GlobalChem()
+    gc.build_global_chem_network()
+    node_1 = list(gc.get_node_smiles('fda_list_one').values())
+    node_2 = list(gc.get_node_smiles('fda_list_two').values())
+    node_3 = list(gc.get_node_smiles('fda_list_three').values())
+    node_4 = list(gc.get_node_smiles('fda_list_four').values())
+    node_5 = list(gc.get_node_smiles('fda_list_five').values())
+    node_6 = list(gc.get_node_smiles('fda_list_six').values())
+    node_7 = list(gc.get_node_smiles('fda_list_seven').values())
+    node_8 = list(gc.get_node_smiles('salt').values())
+    smiles_list = node_1 + node_2 + node_3 + node_4 + node_5 + node_6 + node_7 + node_8
+#     smiles_list = smiles_list + jwh_compounds    
+    
+    for molecule in smiles_list:
+        
+        try:
+
+            rdkit_molecule = Chem.MolFromSmiles(molecule)
+            for i in range(3000):
+                smiles = Chem.MolToSmiles(rdkit_molecule, doRandom=True)
+                output_file.write(smiles + '\n')
+
+        except:
+            continue
+
+
+
