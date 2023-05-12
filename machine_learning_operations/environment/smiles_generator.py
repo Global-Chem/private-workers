7daec8e436cbd14ba5e9a4cab2b36fde627e45a2
Sulstice <sharifsuliman1@gmail.com>
2023-05-12 09:50:23 -0400

    ss-main: universes

---
+++ machine_learning_operations/environment/smiles_generator.py
@@ -0,0 +1,31 @@
+# Imports
+# -------
+
+from rdkit import Chem
+from global_chem import GlobalChem
+
+
+if __name__ == '__main__':
+
+    output_file = open('mol.smi', 'w')
+    gc = GlobalChem()
+    gc.build_global_chem_network()
+    node_1 = list(gc.get_node_smiles('emerging_perfluoroalkyls').values())
+    node_2 = list(gc.get_node_smiles('montmorillonite_adsorption').values())    
+    smiles_list = node_1 + node_2 
+#     smiles_list = smiles_list + jwh_compounds    
+    
+    for molecule in smiles_list:
+        
+        try:
+
+            rdkit_molecule = Chem.MolFromSmiles(molecule)
+            for i in range(1000):
+                smiles = Chem.MolToSmiles(rdkit_molecule, doRandom=True)
+                output_file.write(smiles + '\n')
+
+        except:
+            continue
+
+
+
