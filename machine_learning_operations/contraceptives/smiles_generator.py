2b9987a88f902d3b72c095c47c7593aab57a6d2c
Sulstice <sharifsuliman1@gmail.com>
2023-05-28 10:56:27 -0400

    Added the nodemailer line

---
+++ machine_learning_operations/contraceptives/smiles_generator.py
@@ -0,0 +1,30 @@
+# Imports
+# -------
+
+from rdkit import Chem
+from global_chem import GlobalChem
+
+
+if __name__ == '__main__':
+
+    output_file = open('mol.smi', 'w')
+    gc = GlobalChem()
+    gc.build_global_chem_network()
+    node_1 = list(gc.get_node_smiles('oral_contraceptives').values())
+    smiles_list = node_1 
+#     smiles_list = smiles_list + jwh_compounds    
+    
+    for molecule in smiles_list:
+        
+        try:
+
+            rdkit_molecule = Chem.MolFromSmiles(molecule)
+            for i in range(10000):
+                smiles = Chem.MolToSmiles(rdkit_molecule, doRandom=True)
+                output_file.write(smiles + '\n')
+
+        except:
+            continue
+
+
+
